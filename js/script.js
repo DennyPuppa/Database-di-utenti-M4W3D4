4efebc9382619a78654a75a728e32250b2f53b9b
DennyPuppa <63900736+DennyPuppa@users.noreply.github.com>
2024-03-23 16:40:21 +0100

    Upload exercise

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const url = "https://jsonplaceholder.typicode.com/users"
+const tabBody = document.querySelector("#tab-body-user")
+const inputField = document.querySelector("#user-input")
+
+const createTabs = (id, name, username, email) => {
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    const nameRow = document.createElement("td");
+    const usernameRow = document.createElement("td");
+    const emailRow = document.createElement("td");
+
+    th.scope = "row";
+    th.textContent = id;
+    nameRow.textContent = name;
+    usernameRow.textContent = username;
+    emailRow.textContent = email;
+
+    tr.append(th, nameRow, usernameRow, emailRow);
+    tabBody.append(tr);
+}
+
+const getUser = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+        return data
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getUser().then(res => res.map(user => {
+    createTabs(user.id, user.name, user.username, user.email)
+}))
